897ca22a994837703ddc75261f30603995e5265f
Vitaliy <itvkip@yandex.ru>
2021-04-05 22:01:12 +0300

    Write file.

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ const path = require('path');
 // let file = fs.readFileSync('text.txt', 'utf-8');
 // console.log(file)
 
-fs.readdir('./one/', (err, data) => {
-	if (err){
-		console.log('Ошибка!!!!')
-		console.log(err);
-		return false
-	}
-	console.log(data);
-	data.forEach(item => {
-		console.log(path.extname(item));
-		console.log(fs.statSync(`./one/${item}`));
-	})
+// fs.readdir('./one/', (err, data) => {
+// 	if (err){
+// 		console.log('Ошибка!!!!')
+// 		console.log(err);
+// 		return false
+// 	}
+// 	console.log(data);
+// 	data.forEach(item => {
+// 		console.log(path.extname(item));
+// 		console.log(fs.statSync(`./one/${item}`));
+// 	})
+// });
+
+fs.writeFile('create_file.txt', 'Hello world', (err) => {
+	if(err) console.log(err);
+});
+
+fs.writeFile('./one/create_file.txt', 'Hello world!', (err) => {
+	if(err) console.log(err);
+});
+
+fs.writeFile('./two/create_file.txt', 'Hello world!!!!!', (err) => {
+	if(err) console.log(err);
 });
