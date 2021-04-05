bcff817b0b09f8b1109db9f151972c1965c27409
Vitaliy <itvkip@yandex.ru>
2021-04-05 21:33:32 +0300

    Added 2 way to read file.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const fs = require('fs');
 
-fs.readFile('text.txt', 'utf-8', (err, data) => {
-	console.log(data);
-});
+// fs.readFile('text.txt', 'utf-8', (err, data) => {
+// 	console.log(data);
+// });
+
+let file = fs.readFileSync('text.txt', 'utf-8');
+console.log(file)
