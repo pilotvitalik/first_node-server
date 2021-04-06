4a0fe91e36f4d8a82f2b7f070aab0c6157aa5ef8
Vitaliy <itvkip@yandex.ru>
2021-04-06 10:55:06 +0300

    Write JSON file

--- index.js
+++ index.js
@@ -3,32 +3,12 @@ require('dotenv').config()
 const fs = require('fs');
 const path = require('path');
 
-// fs.readFile('text.txt', 'utf-8', (err, data) => {
-// 	console.log(data);
-// });
+const obj = {
+	name: 'Vitaliy',
+	age: 34,
+	auto: 'Skoda'
+}
 
-// let file = fs.readFileSync('text.txt', 'utf-8');
-// console.log(file)
-
-// fs.readdir('./one/', (err, data) => {
-// 	if (err){
-// 		console.log('Ошибка!!!!')
-// 		console.log(err);
-// 		return false
-// 	}
-// 	console.log(data);
-// 	data.forEach(item => {
-// 		console.log(path.extname(item));
-// 		console.log(fs.statSync(`./one/${item}`));
-// 	})
-// });
-
-fs.writeFile('create_file.txt', 'Hello world', (err) => {
-	if(err) console.log(err);
+fs.writeFile('test.json', JSON.stringify(obj), (err) => {
+	if (err) console.log(err);
 });
-
-fs.writeFile('./one/create_file.txt', 'Hello world!', (err) => {
-	if(err) console.log(err);
-});
-
-//Hello
