de628e6ab8baffac1edf089b6025aebcacedaa79
Vitaliy <itvkip@yandex.ru>
2021-04-07 21:19:13 +0300

    Added module url.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '5.63.152.234';
 const port = 3000;
@@ -6,6 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   if (req.url !== '/favicon.ico'){
     console.log(req.method);
+    console.log(url.parse(req.url));
   }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
