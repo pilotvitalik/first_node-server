2eeb1765ee40ad553109fe01e3c55bcc65a904b7
Vitaliy <itvkip@yandex.ru>
2021-04-07 20:49:41 +0300

    Write some param from request data.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const hostname = '5.63.152.234';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req._connectionKey);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
