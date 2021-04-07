06fefa7b812292e186d5c6e7af368e1a20df078b
Vitaliy <itvkip@yandex.ru>
2021-04-07 20:32:29 +0300

    Create server

--- index.js
+++ index.js
@@ -1,52 +1,6 @@
-require('dotenv').config()
+const http = require('http');
 
-const fs = require('fs');
-const path = require('path');
-const person = require('./test.json');
-const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-
-const results = [];
-
-const obj = {
-	name: 'Vitaliy',
-	age: 34,
-	auto: 'Skoda'
-}
-
-// fs.writeFileSync('test.json', JSON.stringify(obj), (err) => {
-// 	if (err) console.log(err);
-// });
-
-// fs.createReadStream('test1.csv')
-//   .pipe(csv())
-//   .on('data', (data) => results.push(data))
-//   .on('end', () => {
-//     console.log(results);
-//     // [
-//     //   { NAME: 'Daffy Duck', AGE: '24' },
-//     //   { NAME: 'Bugs Bunny', AGE: '22' }
-//     // ]
-//   });
-
-  const csvWriter = createCsvWriter({
-      path: 'test1.csv',
-      header: [
-          {id: 'df_1', title: 'df:'},
-          {id: 'dsfds_1', title: 'dsfds'},
-          {id: 'sdfsdf_1', title: 'sdfsdf'},
-          {id: 'sdfsd_1', title: 'sdfsd'},
-      ]
-  });
-
-  const records = [
-      {df_1: 'Basd',  dsfds_1: 'French, English', sdfsdf_1: 'Hello', sdfsd_1: 'world!'},
-      {df_1: 'Basd',  dsfds_1: 'French', sdfsdf_1: 'Hello', sdfsd_1: 'world!'},
-  ];
-
-  csvWriter.writeRecords(records)       // returns a promise
-      .then(() => {
-          console.log('...Done');
-      });
-
-console.log(person);
+http.createServer((req, res) => {
+  console.log('server work');
+  res.end('Hello world!');
+}).listen(3000);
