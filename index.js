c37a9fabefd61267503fe7cd06ee6822fd61431b
Vitaliy <itvkip@yandex.ru>
2021-04-07 21:46:46 +0300

    Display query data

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const hostname = '5.63.152.234';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const reqURL = url.parse(req.url, true)
   if (req.url !== '/favicon.ico'){
-    console.log(req.method);
-    console.log(url.parse(req.url));
+    console.log(reqURL.query);
+    console.log(reqURL.query.ghjgj)
   }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
